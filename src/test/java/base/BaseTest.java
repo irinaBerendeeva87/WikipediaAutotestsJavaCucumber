9cad38df9948c1fae41e02c7177546087ddf9d5c
irinaBerendeeva87 <irina.berendeeva87@gmail.com>
2023-12-09 13:52:23 +0100

    add BaseTest file with setUp and tearDown methods for driver

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,24 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class BaseTest {
+    public WebDriver driver;
+
+    protected void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    protected void tearDown() {
+        driver.quit();
+    }
+}
